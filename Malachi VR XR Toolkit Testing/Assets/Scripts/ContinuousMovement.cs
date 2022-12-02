39e279ac796e5c795613dcac185aa4afc3643a8d
SharpEye125 <creeperminer125@gmail.com>
2022-12-02 13:50:36 -0800

    Added Joystick movement, but no gravity yet

---
+++ Malachi VR XR Toolkit Testing/Assets/Scripts/ContinuousMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.XR.CoreUtils;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ContinuousMovement : MonoBehaviour
+{
+    public float speed = 1;
+    public XRNode inputSource;
+
+    private XROrigin rig;
+    private Vector2 inputAxis;
+    private CharacterController character;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+        rig = GetComponent<XROrigin>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(inputSource);
+        device.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxis);
+
+    }
+    private void FixedUpdate()
+    {
+        Quaternion headYaw = Quaternion.Euler(0, rig.Camera.transform.eulerAngles.y, 0);
+        Vector3 direction = headYaw * new Vector3(inputAxis.x, 0f, inputAxis.y);
+
+        character.Move(direction * Time.fixedDeltaTime * speed);
+
+    }
+}

118c78b9fdf859ad288019f59d9a452993ed1453
SharpEye125 <creeperminer125@gmail.com>
2022-12-06 11:58:44 -0800

    player may now move and turn
    Other changes:
    Character Collider now properly follows camera/player head along with its height

--- Malachi VR XR Toolkit Testing/Assets/Scripts/ContinuousMovement.cs
+++ Malachi VR XR Toolkit Testing/Assets/Scripts/ContinuousMovement.cs
@@ -9,7 +9,10 @@ public class ContinuousMovement : MonoBehaviour
 {
     public float speed = 1;
     public XRNode inputSource;
-
+    public float gravity = -9.81f;
+    public LayerMask groundLayer;
+    public float additionalHeight = 0.2f;
+    private float fallingSpeed;
     private XROrigin rig;
     private Vector2 inputAxis;
     private CharacterController character;
@@ -30,10 +33,37 @@ public class ContinuousMovement : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        CapsuleFollowHeadset();
+
         Quaternion headYaw = Quaternion.Euler(0, rig.Camera.transform.eulerAngles.y, 0);
         Vector3 direction = headYaw * new Vector3(inputAxis.x, 0f, inputAxis.y);
 
         character.Move(direction * Time.fixedDeltaTime * speed);
 
+        //gravity
+        bool isGrounded = CheckIfGrounded();
+        if (isGrounded)
+        {
+            fallingSpeed = 0;
+        }
+        fallingSpeed += gravity * Time.fixedDeltaTime;
+        character.Move(Vector3.up * fallingSpeed * Time.fixedDeltaTime);
     }
+
+    void CapsuleFollowHeadset()
+    {
+        character.height = rig.CameraInOriginSpaceHeight + additionalHeight;
+        Vector3 capsuleCenter = transform.InverseTransformPoint(rig.Camera.transform.position);
+        character.center = new Vector3(capsuleCenter.x, character.height / 2 + character.skinWidth, capsuleCenter.z);
+    }
+
+    bool CheckIfGrounded()
+    {
+        //Tell us if we're on the ground
+        Vector3 rayStart = transform.TransformPoint(character.center);
+        float rayLength = character.center.y + 0.01f;
+        bool hasHit = Physics.SphereCast(rayStart, character.radius, Vector3.down, out RaycastHit hitInfo, rayLength, groundLayer);
+        return hasHit;
+    }
+
 }

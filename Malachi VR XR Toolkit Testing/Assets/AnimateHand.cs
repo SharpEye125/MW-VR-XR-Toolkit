cdde0d131ce6cb7c0ca8eee60ddda8cfc73b81ab
SharpEye125 <creeperminer125@gmail.com>
2022-12-08 13:59:05 -0800

    To Do: fix script so hands actually animate

--- Malachi VR XR Toolkit Testing/Assets/AnimateHand.cs
+++ Malachi VR XR Toolkit Testing/Assets/AnimateHand.cs
@@ -6,7 +6,7 @@ using UnityEngine.InputSystem;
 public class AnimateHand : MonoBehaviour
 {
     public InputActionProperty pinchAnimationAction;
-
+     
 
     // Start is called before the first frame update
     void Start()

f50d951d2f023d13280af09f4c3bf38d3ce293a6
SharpEye125 <creeperminer125@gmail.com>
2022-12-15 13:33:57 -0800

    Further instantiating testing

--- Malachi VR XR Toolkit Testing/Assets/SampleSimpleInteractableFunc.cs
+++ Malachi VR XR Toolkit Testing/Assets/SampleSimpleInteractableFunc.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class SampleSimpleInteractableFunc : MonoBehaviour
 {
     public GameObject prefab;
     public Transform spawnPoint;
+    public Collider itemSocket;
+    public bool useXRSockets = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +19,18 @@ public class SampleSimpleInteractableFunc : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if((gameObject).tag != "Left Hand" || (gameObject).tag != "Right Hand")
+        {
+            prefab = (gameObject);
+        }
     }
 
     public void SpawnPrefab()
     {
-        Instantiate(prefab);
+        Instantiate(prefab, spawnPoint.position, spawnPoint.rotation);
     }
 }
